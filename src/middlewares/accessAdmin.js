820542f8933a8c355e778e759ebd37761c143949
ManuelDavidRamirez <Manuyagami95@gmail.com>
2022-09-05 19:58:18 -0300

    TP - Middlewares

---
+++ src/middlewares/accessAdmin.js
@@ -0,0 +1,13 @@
+const admins = ["greta", "ada", "vim", "tim"];
+
+const accessAdmin = (req,res,next) => {
+    if (!req.query.user) {
+        return res.redirect("/login")
+    }
+    if (admins.includes(req.query.user.toLowerCase())) {
+        next()
+    }
+    return res.redirect("/no-entry")
+}
+
+module.exports = accessAdmin
---
+++ src/routes/main.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const {index, contact, about, music, admin, login, noEntry} = require("../controllers/mainController");
+
+const accessAdmin = require("../middlewares/accessAdmin");
+
+router
+    .get("/", userLogs,index)
+    .get("/contact", contact)
+    .get("/about", about)
+    .get("/music", music)
+    .get("/admin", accessAdmin, admin)
+    .get("/login", login)
+    .get("/no-entry", noEntry)
+
+module.exports = router;
